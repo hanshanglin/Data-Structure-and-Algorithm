4717d827c566ed29814ba44e9e3c5c488057cbc2
Hans <hanshanglin2017@gmail.com>
2019-04-18 00:15:40 +0800

    modify insert search & delete

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -27,14 +27,34 @@ public:
         sentinel->prev = sentinel;
     }
 
-    insert(T value){
+    void insert(T value){
         //add at end
         Node *newNode = new Node(value, sentinel->prev, sentinel);
         sentinel->prev->next = newNode;
         sentinel->prev = newNode;
     }
 
-    
+    bool insert(T key){
+        Node *cur = search(key);
+        if (cur==nullptr)
+            return false;
+        cur->prev->next = cur->next;
+        cur->next->prev = cur->prev;
+        free(cur);
+        return true;
+    }
+
+    Node* search(T key){
+        // set sentinel's value to key
+        sentinel->value = key;
+        Node *cur = sentinel->next;
+        while (cur->value.equal(key))
+            cur = cur->next;
+        if(cur == sentinel)
+            return nullptr;
+        return cur;
+    }
+
     ~DoublyLinkedList(){
         free(sentinel);
     }
